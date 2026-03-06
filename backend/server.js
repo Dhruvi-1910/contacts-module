f9d9c2235345f40c93937845af98aa27b97382fd
Dhruvi-1910 <dhruvishah191004@gmail.com>
2026-03-06 22:28:09 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const contactRoutes = require("./routes/contactRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/crmDB")
+.then(()=> console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.use("/api/contacts", contactRoutes);
+
+app.listen(5000, () => {
+console.log("Server running on port 5000");
+});
